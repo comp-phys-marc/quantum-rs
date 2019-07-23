ba90514845d19d14be7f6c826e4ecb7d0550a2e8
Marcus Edwards <marcusedwards@hotmail.ca>
2019-07-23 15:06:07 -0400

    Make client the main

--- src/main.rs
+++ src/main.rs
@@ -1,236 +1,89 @@
-extern crate bit_vec;
-use bit_vec::BitVec;
-use std::time::Instant;
+extern crate dotenv;
+extern crate qasm;
+
+use crate::parser;
+
+use std::env;
 use std::collections::BTreeMap;
-mod ket;
-mod state;
-mod coefficient;
+use amiquip::{
+    AmqpProperties, Connection, ConsumerMessage, ConsumerOptions, Exchange, Publish,
+    QueueDeclareOptions, Result
+};
 
-fn create_eleven_bit_ket() -> ket::Ket {
-    let imaginary_coeff = coefficient::create_coefficient(1.0, true);
-    let real_coeff = coefficient::create_coefficient(1.0, false);
-    let complex_coeff = coefficient::create_complex_coefficient(real_coeff, imaginary_coeff);
-    let init_state = BitVec::from_elem(11, false);
-    let ket = ket::create_ket(complex_coeff, init_state.clone(), vec![]);
-    ket
-}
 
-fn ten_qubit_counterfeit_coin_finding() -> BitVec {
+fn main() -> Result<()> {
 
-    let num_qubits = 10;
-    let symbol = 'q';
-    let mut state = state::create_state(vec![create_eleven_bit_ket()], num_qubits, symbol);
-    let mut creg = BitVec::from_elem(10, false);
+    dotenv::dotenv();
 
-    state.h(0);
-    state.h(1);
-    state.h(2);
-    state.h(3);
-    state.h(4);
-    state.h(5);
-    state.h(6);
-    state.h(7);
-    state.h(8);
+    let rb_user = env::var("RABBIT_USER").expect("Queue user not configured!");
+    let rb_pass = env::var("RABBIT_PASSWORD").expect("Queue password not configured!");
+    let rb_port = env::var("RABBIT_PORT").expect("Queue port not configured!");
+    let rb_host = env::var("RABBIT_HOST").expect("Queue host not configured!");
 
-    state.cx(0, 10);
-    state.cx(1, 10);
-    state.cx(2, 10);
-    state.cx(3, 10);
-    state.cx(4, 10);
-    state.cx(5, 10);
-    state.cx(6, 10);
-    state.cx(7, 10);
-    state.cx(8, 10);
+    // Open RabbitMQ connection.
+    let mut connection = Connection::insecure_open(&format!("amqp://{}:{}@{}:{}", rb_user, rb_pass, rb_host, rb_port))?;
 
-    let result = state.m(9);
-    creg.set(9, result);
+    // Open a channel - None lets the library choose the channel ID.
+    let channel = connection.open_channel(None)?;
 
-    if result == false { state.x(9); }
-    if result == false { state.h(9); }
-    if result == true { state.h(0); }
-    if result == true { state.h(1); }
-    if result == true { state.h(2); }
-    if result == true { state.h(3); }
-    if result == true { state.h(4); }
-    if result == true { state.h(5); }
-    if result == true { state.h(6); }
-    if result == true { state.h(7); }
-    if result == true { state.h(8); }
-    if result == false { state.cx(6, 9); }
-    if result == false { state.h(0); }
-    if result == false { state.h(1); }
-    if result == false { state.h(2); }
-    if result == false { state.h(3); }
-    if result == false { state.h(4); }
-    if result == false { state.h(5); }
-    if result == false { state.h(6); }
-    if result == false { state.h(7); }
-    if result == false { state.h(8); }
+    // Get a handle to the default direct exchange.
+    let exchange = Exchange::direct(&channel);
 
-    state.normalize();
+    // Declare the queue that will receive RPC requests.
+    let queue = channel.queue_declare("rpc_queue", QueueDeclareOptions::default())?;
 
-    let mut q = 0;
-    while q < 10 {
-        creg.set(q, state.m(q));
-        q += 1;
+    // Start a consumer.
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    println!("Awaiting RPC requests...");
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("({:>3}) fib({})", i, body);
+
+                let (reply_to, corr_id) = match (
+                    delivery.properties.reply_to(),
+                    delivery.properties.correlation_id(),
+                ) {
+                    (Some(r), Some(c)) => (r.clone(), c.clone()),
+                    _ => {
+                        println!("received delivery without reply_to or correlation_id");
+                        consumer.ack(delivery)?;
+                        continue;
+                    }
+                };
+
+                let mut response:String = "{".to_string();
+                let result = parser::execute_qasm(&body);
+                let regs: Vec<BTreeMap<usize, usize>> = result.values().cloned().collect();
+                let keys:Vec<char> = result.keys().cloned().collect();
+
+                let mut i = 0;
+                for reg in regs {
+                    response.push_str(&format!(" \"{}\": ", keys[i]));
+                    let bitstring:Vec<usize> = reg.values().cloned().collect();
+                    for bit in bitstring {
+                        response.push_str(bit.to_string().as_str());
+                    }
+                    response.push_str(",");
+                    i += 1;
+                }
+                response.push_str("}");
+
+                exchange.publish(Publish::with_properties(
+                    response.as_bytes(),
+                    reply_to,
+                    AmqpProperties::default().with_correlation_id(corr_id),
+                ))?;
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
     }
-    creg
-}
 
-fn eleven_qubit_counterfeit_coin_finding() -> BitVec {
-
-    let num_qubits = 11;
-    let symbol = 'q';
-    let mut state = state::create_state(vec![create_eleven_bit_ket()], num_qubits, symbol);
-    let mut creg = BitVec::from_elem(11, false);
-
-    state.h(0);
-    state.h(1);
-    state.h(2);
-    state.h(3);
-    state.h(4);
-    state.h(5);
-    state.h(6);
-    state.h(7);
-    state.h(8);
-    state.h(9);
-
-    state.cx(0, 10);
-    state.cx(1, 10);
-    state.cx(2, 10);
-    state.cx(3, 10);
-    state.cx(4, 10);
-    state.cx(5, 10);
-    state.cx(6, 10);
-    state.cx(7, 10);
-    state.cx(8, 10);
-    state.cx(9, 10);
-
-    let result = state.m(10);
-    creg.set(10, result);
-
-    if result == false { state.x(10); }
-    if result == false { state.h(10); }
-    if result == true { state.h(0); }
-    if result == true { state.h(1); }
-    if result == true { state.h(2); }
-    if result == true { state.h(3); }
-    if result == true { state.h(4); }
-    if result == true { state.h(5); }
-    if result == true { state.h(6); }
-    if result == true { state.h(7); }
-    if result == true { state.h(8); }
-    if result == true { state.h(9); }
-    if result == false { state.cx(6, 10); }
-    if result == false { state.h(0); }
-    if result == false { state.h(1); }
-    if result == false { state.h(2); }
-    if result == false { state.h(3); }
-    if result == false { state.h(4); }
-    if result == false { state.h(5); }
-    if result == false { state.h(6); }
-    if result == false { state.h(7); }
-    if result == false { state.h(8); }
-    if result == false { state.h(9); }
-
-    state.normalize();
-
-    let mut q = 0;
-    while q < 11 {
-        creg.set(q, state.m(q));
-        q += 1;
-    }
-    creg
-}
-
-fn twelve_qubit_counterfeit_coin_finding() -> BitVec {
-
-    let num_qubits = 12;
-    let symbol = 'q';
-    let mut state = state::create_state(vec![create_eleven_bit_ket()], num_qubits, symbol);
-    let mut creg = BitVec::from_elem(12, false);
-
-    state.h(0);
-    state.h(1);
-    state.h(2);
-    state.h(3);
-    state.h(4);
-    state.h(5);
-    state.h(6);
-    state.h(7);
-    state.h(8);
-    state.h(9);
-    state.h(10);
-
-
-    state.cx(0, 11);
-    state.cx(1, 11);
-    state.cx(2, 11);
-    state.cx(3, 11);
-    state.cx(4, 11);
-    state.cx(5, 11);
-    state.cx(6, 11);
-    state.cx(7, 11);
-    state.cx(8, 11);
-    state.cx(9, 11);
-    state.cx(10, 11);
-
-    let result = state.m(11);
-    creg.set(11, result);
-
-    if result == false { state.x(10); }
-    if result == false { state.h(10); }
-    if result == true { state.h(0); }
-    if result == true { state.h(1); }
-    if result == true { state.h(2); }
-    if result == true { state.h(3); }
-    if result == true { state.h(4); }
-    if result == true { state.h(5); }
-    if result == true { state.h(6); }
-    if result == true { state.h(7); }
-    if result == true { state.h(8); }
-    if result == true { state.h(9); }
-    if result == false { state.cx(6, 10); }
-    if result == false { state.h(0); }
-    if result == false { state.h(1); }
-    if result == false { state.h(2); }
-    if result == false { state.h(3); }
-    if result == false { state.h(4); }
-    if result == false { state.h(5); }
-    if result == false { state.h(6); }
-    if result == false { state.h(7); }
-    if result == false { state.h(8); }
-    if result == false { state.h(9); }
-
-    state.normalize();
-
-    let mut q = 0;
-    while q < 11 {
-        creg.set(q, state.m(q));
-        q += 1;
-    }
-    creg
-}
-
-
-fn main() {
-    let mut start = Instant::now();
-    let mut output = ten_qubit_counterfeit_coin_finding();
-    let mut elapsed = start.elapsed();
-    println!("Elapsed time for 10 qubits: {} ms",
-             (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64);
-
-    start = Instant::now();
-    output = eleven_qubit_counterfeit_coin_finding();
-    elapsed = start.elapsed();
-    println!("Elapsed time for 11 qubits: {} ms",
-             (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64);
-
-    start = Instant::now();
-    output = twelve_qubit_counterfeit_coin_finding();
-    elapsed = start.elapsed();
-    println!("Elapsed time for 12 qubits: {} ms",
-             (elapsed.as_secs() * 1_000) + (elapsed.subsec_nanos() / 1_000_000) as u64);
-
-    
+    connection.close()
 }
